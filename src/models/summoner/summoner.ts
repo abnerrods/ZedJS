2f2abb7d8a1dbf771d09f277641dd40a6dd0fff9
João Lenon <lenonSec7@hotmail.com>
2020-03-20 23:14:10 -0300

    feat: 📝 Constants and Models

---
+++ src/models/summoner/summoner.ts
@@ -0,0 +1,9 @@
+export class SummonerV4 {
+    public readonly id: string
+    public readonly name: string
+    public readonly puuid: string
+    public readonly accountId: string
+    public readonly revisionDate: number
+    public readonly summonerLevel: number
+    public readonly profileIconId: number
+}
