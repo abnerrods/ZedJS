5686341cd811eca6f7dbb771b1104f369c596c25
João Lenon <lenonSec7@hotmail.com>
2020-03-21 00:33:12 -0300

    feat: 📝 Constants

---
+++ src/constants/regions.ts
@@ -0,0 +1,14 @@
+export const enum Regions {
+  BRAZIL = 'br1',
+  EU_EAST = 'eun1',
+  EU_WEST = 'euw1',
+  KOREA = 'kr',
+  LAT_NORTH = 'la1',
+  LAT_SOUTH = 'la2',
+  AMERICA_NORTH = 'na1',
+  OCEANIA = 'oc1',
+  TURKEY = 'tr1',
+  RUSSIA = 'ru',
+  JAPAN = 'jp1',
+  PBE = 'pbe'
+}
